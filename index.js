b0c7828c8f806b6a44bea83ce2a2643353d7077f
Anushkak02 <110720811+Anushkak02@users.noreply.github.com>
2023-06-21 19:35:33 +0530

    Added the javascript file
    index.js file

---
+++ index.js
@@ -0,0 +1,14 @@
+
+$(".btn").click(function() {
+    alert("You will directly be taken to the site's webpage.");
+});
+
+$(".mail").click(function() {
+    alert("You'll be directed to your mail applicattion.");
+});
+
+$(".insta").click(function() {
+    alert("You'll be directed to my instagram profile!");
+});
+
+
